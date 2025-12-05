a62a90ea3661601b26f192ca18c05cab9290a780
nguyenhuuanhdev <nguyenhuuanhvippro@gmail.com>
2025-12-05 13:25:16 +0700

    Mô tả thay đổi

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener("install", () => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", async () => {
+    // Xoá hết cache
+    const keys = await caches.keys();
+    for (const key of keys) await caches.delete(key);
+
+    // Hủy đăng ký SW
+    self.clients.matchAll().then(clients => {
+        clients.forEach(client => client.navigate(client.url));
+    });
+});
